f741fca85395873b952138050b65b7037fc31934
stckrz <rin449@gmail.com>
2024-12-29 13:56:25 -0600

    added socketio integration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import http from 'http';
 import fs from 'fs';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
@@ -7,11 +8,21 @@ import { shuffle } from './lib/commonStuff.js';
 import { getSubDirectoriesToWrite } from './lib/writeFile.js';
 import { Song } from './Classes/Playable/Song.js';
 import { PlaybackManager } from './Classes/PlaybackManager.js';
+import { Server as SocketIOServer } from 'socket.io'; // Import Socket.IO
 
 dotenv.config()
 const app = express();
 app.use(cors())
+
 const PORT = process.env.NODE_PORT
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+	cors: {
+		origin: '*', // Allow all origins, or specify your frontend's URL
+		methods: ['GET', 'POST'],
+	},
+});
 const queue: Song[] = [];
 export const loadQueueFromFile = async (filePath: string) => {
 	try {
@@ -30,6 +41,33 @@ export const loadQueueFromFile = async (filePath: string) => {
 		}
 	}
 };
+const returnDirectoryContents = (filePath: string) => {
+	try {
+
+		if (!fs.existsSync(filePath)) {
+			console.log("directory does not exist", filePath);
+			return
+		}
+		if (filePath === './audio') {
+			// fs.writeFileSync(outputFile, '', 'utf8');
+		}
+
+		const files = fs.readdirSync(filePath, { withFileTypes: true });
+		const directories = files.filter((file) => file.isDirectory())
+		const mp3Files = files.filter((file) => file.name.endsWith('mp3'))
+
+		return ({ directories: directories, files: mp3Files })
+		// directories.forEach((subdirectory) => {
+		// 	const subDirectoryPath = `${filePath}/${subdirectory.name}`
+		// })
+	}
+	catch (err) {
+		if (err instanceof Error) {
+			console.log('Error reading directories', err.message)
+		}
+	}
+
+}
 
 const songFilePath = path.join(process.cwd(), 'songFiles.txt');
 //function that adds the songs to the songFile
@@ -41,10 +79,17 @@ const playbackManager = new PlaybackManager(queue);
 	await loadQueueFromFile(songFilePath); // Load initial songs
 	playbackManager.playNextSong();       // Start playback
 })();
-
+// res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//    res.setHeader('Content-Type', 'audio/mpeg');
 //on connect, streams the piped audio to the req. with headers
-app.get('/stream', (req, res) => {
+app.get('/stream', cors(), (req, res) => {
 	res.writeHead(200, {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		'Access-Control-Allow-Methods': 'GET, OPTIONS',
+
 		'Content-Type': 'audio/mpeg',
 		'Transfer-Encoding': 'chunked',
 		'Connection': 'keep-alive',
@@ -56,6 +101,7 @@ app.get('/stream', (req, res) => {
 	//disconnects the stream from the user when they close the connection
 	req.on('close', () => {
 		playbackManager.getStreamBuffer().unpipe(res);
+		console.log('Client disconnected');
 	})
 })
 app.get('/skip', (req, res) => {
@@ -64,10 +110,38 @@ app.get('/skip', (req, res) => {
 
 })
 
-app.get('/metadata', cors(),(req, res) => {
+app.get('/metadata', cors(), (req, res) => {
 	res.json(playbackManager.currentSongMetadata());
 });
 
-app.listen(3000, '0.0.0.0', () => {
+io.on('connection', (socket) => {
+	console.log("a user connected!", socket.id);
+	socket.emit('currentSong', playbackManager.currentSongMetadata())
+
+	socket.on('currentSong', () => {
+		socket.emit('currentSong', playbackManager.currentSongMetadata())
+	})
+
+	socket.on('listContents', (data, callback) => {
+		const filePath = data.filePath
+		const directoryContents = returnDirectoryContents(filePath)
+		callback(directoryContents)
+		// socket.emit('directoryList', returnDirectoryContents(filePath));
+	})
+
+	socket.on('addSong', (data) => {
+		console.log("filepath", data.filePath)
+		const songPath = data.songPath
+		if (songPath) {
+			queue.push(new Song(songPath))
+		}
+	})
+
+	socket.on('disconnect', () => {
+		console.log("a user disconnected!", socket.id)
+	})
+})
+
+server.listen(3000, '0.0.0.0', () => {
 	console.log(`Server running at http://localhost:${PORT}`);
 });
